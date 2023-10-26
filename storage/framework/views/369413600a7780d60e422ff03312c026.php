157a38e7f88c12161e5811df399a078d242739ce
JoseSantiagoVillalba <129118050+JoseSantiagoVillalba@users.noreply.github.com>
2023-10-26 11:52:12 -0300

    Add files via upload

---
+++ storage/framework/views/369413600a7780d60e422ff03312c026.php
@@ -0,0 +1,5 @@
+
+<div class="left message">
+    <img src="https://st2.depositphotos.com/6473186/9838/i/450/depositphotos_98388668-stock-photo-chicken-isolated-on-white-background.jpg" alt="avatar">
+    <p> <?php echo e($message); ?> </p>
+</div><?php /**PATH C:\Users\elcor\OneDrive\Escritorio\Programación\LARAVEL\ProyectoPrueba1\Chat 2.0\Chat2.0\resources\views/receive.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/38b37e534c359871908bff3165fea51c.php
@@ -0,0 +1,5 @@
+
+<div class="right message">
+<?php echo $message ?>
+    <img src="https://st2.depositphotos.com/6473186/9838/i/450/depositphotos_98388668-stock-photo-chicken-isolated-on-white-background.jpg" alt="avatar">
+</div><?php /**PATH C:\Users\elcor\OneDrive\Escritorio\Programación\LARAVEL\ProyectoPrueba1\Chat 2.0\Chat2.0\resources\views/broadcast.blade.php ENDPATH**/ ?>
